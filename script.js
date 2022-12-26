d0af39b0561a990df08d7897d2991eb37e0d86de
nvizcarra <nicolasvizcarra64@gmail.com>
2022-12-26 01:38:00 -0300

    Fusion menu mobile

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// #######################################################
-// #1 selecciono navbar-email
+// #####################VARIABLES##################################
+// #1 selecciono clase css navbar-email
 const ordersAndProfile = document.querySelector('.navbar-email');
 // #4 selecciono desktop-menu
 const desktopMenu = document.querySelector('.desktop-menu');
+// #6 selecciono clase css menu (botón hamburguesa)
+const burgerButton = document.querySelector('.menu'); // revisar
+// #7 selecciono .mobile-menu
+const mobileMenu = document.querySelector('.mobile-menu');
 
-// #######################################################
-// #2 escucho acción del usuario
+// ######################EVENTOS#################################
+// #2 escucho acción del usuario desktop
 ordersAndProfile.addEventListener('click', toggleOrdersAndProfile);
+// #8 escucho acción del usuario mobile
+burgerButton.addEventListener('click', toggleMobileMenu);
 
+
+// ######################FUNCIONES#################################
 // #3 Función de toggle
 function toggleOrdersAndProfile() {
-    // #5 ejecuto toggle active
+    // #5 ejecuto toggle
     desktopMenu.classList.toggle('inactive');
 }
 
-console.log('hola');
+// #9 Toggle
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+}
